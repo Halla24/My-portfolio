5207ec7e5c436c279867da5f3e50eeb58221c405
halla <aniabisso.16@gmail.com>
2023-12-06 20:36:19 +0100

    fixing some issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,51 @@
-import React from 'react';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import React from "react";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   const openLink = (url) => {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
 
   return (
     <footer className="bg-purple-200 p-4 md:p-6 lg:p-8 text-purple-800 font-serif ">
       <div className="flex flex-col md:flex-row justify-between items-center max-w-full mx-auto px-20">
-
         <div className="mb-2 md:mb-0">
-          <img src="logo.png" alt="Logo" className="max-w-full md:max-w-[120px]" />
+          <img
+            src="logo.png"
+            alt="Logo"
+            className="max-w-full md:max-w-[120px]"
+          />
         </div>
 
         <div className="flex items-center">
-          <div className="text-purple-800 mx-2" onClick={() => openLink('https://github.com/Halla24')}>
+          <div
+            className="text-purple-800 cursor-pointer mx-2"
+            onClick={() => openLink("https://github.com/Halla24")}
+          >
             <FaGithub size={20} />
           </div>
-          <div className="text-purple-800 mx-2" onClick={() => openLink('https://www.linkedin.com/in/halla-hamidi-989197229/')}>
+          <div
+            className="text-purple-800 cursor-pointer mx-2"
+            onClick={() =>
+              openLink("https://www.linkedin.com/in/halla-hamidi-989197229/")
+            }
+          >
             <FaLinkedin size={20} />
           </div>
-          <div className="text-purple-800 mx-2" onClick={() => openLink('https://mail.google.com/mail/u/0/?fs=1&to=aniabisso.16@gmail.com&tf=cm')}>
+          <div
+            className="text-purple-800 cursor-pointer mx-2"
+            onClick={() =>
+              openLink(
+                "https://mail.google.com/mail/u/0/?fs=1&to=aniabisso.16@gmail.com&tf=cm"
+              )
+            }
+          >
             <FaEnvelope size={20} />
           </div>
         </div>
-
       </div>
     </footer>
   );
 };
 
 export default Footer;
-
-
-
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const ProjectCard = ({ title, imageSrc, projectLink }) => {
   const openProjectLink = () => {
@@ -15,16 +15,16 @@ const ProjectCard = ({ title, imageSrc, projectLink }) => {
         />
         <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
           <div className="bg-gray-800 bg-opacity-50 w-full h-full absolute inset-0"></div>
-          <div className="text-white absolute z-10">
-           <div
-           className="bg-[#4B2499]  text-white font-serif  px-4 py-2 rounded-md transition-all duration-300 transform hover:scale-105 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl"
-           onClick={openProjectLink}
-           >
-          Check the Project
-         </div>
-         <div className="text-purple-200 font-serif  mt-4 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-           Next.JS / Tailwind CSS
-           </div>
+          <div className="text-white flex flex-col items-center absolute z-10">
+            <div
+              className="bg-[#4B2499] hover:bg-[#4B2499] w-[70%] text-white font-serif text-center self-center rounded-md transition-all duration-300 transform hover:scale-105 text-sm sm:text-base md:text-md"
+              onClick={openProjectLink}
+            >
+              Check the Project
+            </div>
+            <div className="text-purple-200 font-serif  mt-4 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+              Next.JS / Tailwind CSS
+            </div>
           </div>
         </div>
       </div>
@@ -35,20 +35,24 @@ const ProjectCard = ({ title, imageSrc, projectLink }) => {
 const Projects = () => {
   return (
     <div className="max-w-4xl mx-auto mt-12">
-      <div className="flex flex-wrap justify-center gap-8 lg:justify-between">
-        <div className="flex gap-8">
-          <ProjectCard
-            title="Movies website"
-            imageSrc="./project1.png"
-            projectLink="https://reelio-topaz.vercel.app/"
-          />
-          <ProjectCard
-            title="E-commerce website"
-            imageSrc="./project2.png"
-            projectLink="https://sougna.vercel.app/"
-          />
+      <div className="flex flex-wrap justify-center items-center gap-8 lg:justify-between">
+        <div className="flex flex-wrap items-center justify-center gap-8">
+          <div className="w-96 mobileWidth">
+            <ProjectCard
+              title="Movies website"
+              imageSrc="./project1.png"
+              projectLink="https://reelio-topaz.vercel.app/"
+            />
+          </div>
+          <div className="w-96 mobileWidth">
+            <ProjectCard
+              title="E-commerce website"
+              imageSrc="./project2.png"
+              projectLink="https://sougna.vercel.app/"
+            />
+          </div>
         </div>
-        <div className="flex mx-auto items-center w-96 justify-center mt-8">
+        <div className="flex mx-auto mobileWidth items-center w-96 justify-center">
           <ProjectCard
             title="Volunteer events website"
             imageSrc="./project3.png"
@@ -61,6 +65,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
-
-
