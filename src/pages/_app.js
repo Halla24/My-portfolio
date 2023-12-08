4f70f32410e644aeb169d17195a41b7dc16c0087
halla <aniabisso.16@gmail.com>
2023-12-08 22:00:46 +0100

    enhance SEO and add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,43 @@
-import '@/styles/globals.css'
+import { DefaultSeo } from "next-seo";
+import "@/styles/globals.css";
 import "../styles/scss/index.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="Halla Hamidi - Front-End Web Developer"
+        description="Portfolio of Halla Hamidi, a proficient front-end web developer specializing in JavaScript, Tailwind, Next.js, CSS, HTML, SEO and Firebase. Explore my projects and get in touch for collaboration."
+        additionalMetaTags={[
+          {
+            name: "keywords",
+            content:
+              "JavaScript, Halla Hamidi, React, Tailwind, Front-End Web Developer, Next.js, CSS, HTML, Firebase, SEO, Portfolio",
+          },
+          {
+            name: "author",
+            content: "Halla Hamidi",
+          },
+        ]}
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://halla-hamidi.vercel.app/",
+          site_name: "halla-hamidi",
+          title: "Halla Hamidi - Front-End Web Developer",
+          description:
+            "Portfolio of Halla Hamidi, a proficient front-end web developer specializing in JavaScript, Tailwind, Next.js, CSS, SEO, HTML, and Firebase. Explore my projects and get in touch for collaboration.",
+          images: [
+            {
+              url: "/butterfly.png",
+              width: 800,
+              height: 600,
+              alt: "Halla",
+            },
+          ],
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
